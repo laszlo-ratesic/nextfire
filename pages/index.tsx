060bb7de907f5e371d8f8c3bc159284569c3b632
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-06-16 16:34:43 -0500

    create custom loading spinner to manage loading states across app

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,21 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
+import Loader from '../components/Loader';
+
 export default function Home() {
   return (
+    // <div>
+    //   <Link prefetch={false} href={{
+    //     pathname: '/[username]',
+    //     query: { username: 'laszlo-ratesic' },
+    //   }}>
+    //     <a>Keenan&apos;s Profile</a>
+    //   </Link>
+    // </div>
+
     <div>
-      <Link prefetch={false} href={{
-        pathname: '/[username]',
-        query: { username: 'laszlo-ratesic' },
-      }}>
-        <a>Keenan&apos;s Profile</a>
-      </Link>
+      <Loader show />
     </div>
   )
 }
