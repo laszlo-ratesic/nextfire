aaa11615365e9a3b4d72c9672175c07ee89bf33f
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-06-16 07:57:04 -0500

    initialize firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import firebase, { initializeApp } from "firebase/firebase-app-compat";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import 'firebase/auth-compat';
+import 'firebase/firestore-compat';
+import 'firebase/storage-compat';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8K3avroeTalf80hnNnXtCvojFpaWpzwQ",
+  authDomain: "nextfire-c4d11.firebaseapp.com",
+  projectId: "nextfire-c4d11",
+  storageBucket: "nextfire-c4d11.appspot.com",
+  messagingSenderId: "857102046174",
+  appId: "1:857102046174:web:c1edf763ad52726e3b034f",
+  measurementId: "G-1M0GLTQW8H"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+if (!firebase.getApps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
