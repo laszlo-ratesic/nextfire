12b82bf878bbaea10d0b3976b9299a82d4709db3
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-06-17 17:49:27 -0500

    introduce react-hot-toast

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-
 import Loader from '../components/Loader';
+import toast from 'react-hot-toast';
 
 export default function Home() {
   return (
-    // <div>
-    //   <Link prefetch={false} href={{
-    //     pathname: '/[username]',
-    //     query: { username: 'laszlo-ratesic' },
-    //   }}>
-    //     <a>Keenan&apos;s Profile</a>
-    //   </Link>
-    // </div>
-
     <div>
-      <Loader show />
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast Me
+      </button>
     </div>
   )
 }
