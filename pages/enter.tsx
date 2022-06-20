310011e3cef356690f360dc6eb503bd182879bd8
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-06-20 12:59:38 -0500

    implement custom usernames for firebase users and asynchronously validate uniqueness

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,8 +1,9 @@
-import { useContext } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { UserContext } from '../lib/context';
-import { auth, googleAuthProvider } from '../lib/firebase';
+import { auth, firestore, googleAuthProvider } from '../lib/firebase';
+import debounce from 'lodash.debounce';
 
-export default function EnterPage(props) {
+export default function EnterPage(...props) {
   const { user, username } = useContext(UserContext);
 
   // 1. user signed out <SignInButton />
@@ -50,5 +51,113 @@ function SignOutButton() {
 }
 
 function UsernameForm() {
-  return <></>;
+  const [formValue, setFormValue] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user, username } = useContext(UserContext);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create refs for both documents
+    const userDoc = firestore.doc(`users/${user.uid}`);
+    const usernameDoc = firestore.doc(`usernames/${formValue}`);
+
+    // Commit both docs together as a batch write
+    const batch = firestore.batch();
+    batch.set(userDoc, {
+      username: formValue,
+      photoURL: user.photoURL,
+      displayName: user.displayName,
+    });
+    batch.set(usernameDoc, { uid: user.uid });
+
+    try {
+      await batch.commit();
+    } catch(err) {
+      console.error(err);
+    }
+  };
+
+  const onChange = (e) => {
+    // Force form value typed in form to match correct format
+    const val = e.target.value.toLowerCase();
+    const regex = /^(?=[a-zA-Z0-9._-]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    // Only set form value if length is < 3 OR it passes regex
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (regex.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  };
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue]);
+
+  // Hit the db for username match after each debounced change
+  // useCallback required for debounce
+  const checkUsername = useCallback(
+    debounce(async (username) => {
+      if (username.length >= 3) {
+        const ref = firestore.doc(`usernames/${username}`);
+        const { exists } = await ref.get();
+        console.log('Firestore read executed');
+        setIsValid(!exists);
+        setLoading(false);
+      }
+    }, 500),
+    []
+  );
+
+  return (
+    !username && (
+      <section>
+        <h3>Choose Username</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            name="username"
+            placeholder="username"
+            value={formValue}
+            onChange={onChange}
+          />
+
+          <UsernameMessage username={formValue} isValid={isValid} loading={loading} />
+
+          <button type="submit" className="btn-green" disabled={!isValid}>
+            Choose
+          </button>
+
+          <h3>Debug State</h3>
+          <div>
+            Username: {formValue}
+            <br />
+            Loading: {loading.toString()}
+            <br />
+            Username Valid: {isValid.toString()}
+          </div>
+        </form>
+      </section>
+    )
+  );
+}
+
+function UsernameMessage({ username, isValid, loading}) {
+  if (loading) {
+    return <p>Checking...</p>;
+  } else if (isValid) {
+    return <p className='text-success'>{username} is available!</p>
+  } else if (username && !isValid) {
+    return <p className="text-danger">That username is taken!</p>
+  } else {
+    return <p></p>
+  }
 }
