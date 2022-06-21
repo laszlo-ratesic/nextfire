11910d498cdc87d03a575a529b06d81de7996005
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-06-21 18:26:10 -0500

    implement SSR to fetch user profile page data from server

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -40,3 +40,30 @@ export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 //   functions.useFunctionsEmulator('http://localhost:5001');
 // }
+
+/// HELPER FUNCTIONS
+
+/**
+ * Gets a users/{uid} document with username
+ * @param {string} username
+ */
+export async function getUserWithUsername(username) {
+  const usersRef = firestore.collection('users');
+  const query = usersRef.where('username', '==', username).limit(1);
+  const userDoc = (await query.get()).docs[0];
+  return userDoc;
+}
+
+/**
+ * Converts a firestore document to JSON
+ * @param {DocumentSnapshot} doc
+ */
+export function postToJSON(doc) {
+  const data = doc.data();
+  return {
+    ...data,
+    // Gotcha! firestore timestamp NOT serializable to JSON
+    createdAt: data.createdAt.toMillis(),
+    updatedAt: data.updatedAt.toMillis(),
+  }
+}
